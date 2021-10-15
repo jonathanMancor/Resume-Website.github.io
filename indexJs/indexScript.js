7d8375d2a6c1234c15fecf5973ca370034c7847c
jonathanMancor <77824159+jonathanMancor@users.noreply.github.com>
2021-10-14 22:40:53 -0400

    Add files related to landing page (index.html)

---
+++ indexJs/indexScript.js
@@ -0,0 +1,89 @@
+var okiDoor = document.getElementById('okiDoor').src;
+var mancorDoor = document.getElementById('mancorDoor').src;
+
+
+
+
+
+
+
+//open oki preface
+$('#okiDoor').click(function () {
+  $('#blackScreen').css('display', 'block');
+
+  $('#blackScreen').fadeTo( 500, 1.0 );
+  $('#doorDisplay').css('display', 'none');
+  $('#okiPreface').css('display', 'flex');
+  $('body').css('color', 'white');
+});
+
+
+//oki door animations
+$('#okiDoor').mouseover(function () {
+  $('#okiDoor > img').attr('src', 'indexImg/okiDoorFrames/okiDoorOpen.gif');
+  if ( okiDoor = 'indexImg/okiDoorFrames/okiDoorOpen.gif' ) {
+    return
+  }
+});
+$('#okiDoor').mouseout(function () {
+  $('#okiDoor > img').attr('src', 'indexImg/okiDoorFrames/okiDoorClose.gif');
+  if ( okiDoor = 'indexImg/okiDoorFrames/okiDoorClose.gif' ) {
+    return
+  }
+});
+
+//play oki yes
+$('#playYes').click(function () {
+  location.assign("indexProjects/oki-v5.3/home.html");
+});
+
+//play oki no
+$('#playNo').click(function () {
+  $('#blackScreen').fadeTo( 500, 0 );
+
+  $('#okiPreface').css('display', 'none');
+  $('body').css('color', 'black');
+  setTimeout(function(){
+    $('#doorDisplay').css('display', 'flex');
+  }, 200);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//mancor door animations
+$('#mancorDoor').mouseover(function () {
+  $('#mancorDoor > img').attr('src', 'indexImg/mancorDoorFrames/mancorDoorOpen.gif');
+  if ( mancorDoor = 'indexImg/mancorDoorFrames/mancorDoorOpen.gif' ) {
+    return
+  }
+});
+$('#mancorDoor').mouseout(function () {
+  $('#mancorDoor > img').attr('src', 'indexImg/mancorDoorFrames/mancorDoorClose.gif');
+  if ( mancorDoor = 'indexImg/mancorDoorFrames/mancorDoorClose.gif' ) {
+    return
+  }
+});
+
+$('#mancorDoor').click(function () {
+  location.assign("indexProjects/portfolio-2021/indexPortfolio.html");
+});
